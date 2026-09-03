e8e1b3f84c7a2b80f3be094537f617e125b15321
CHALEX-HUB <german.camarenaofg@gmail.com>
2026-09-03 15:06:35 -0500

    asdfadf

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def read_config_sheet(sheet_name):
 
 st.set_page_config(page_title="CHALEX-MDA", page_icon="🐺", layout="wide")
 try:
-    st.image("chalex_network.png", width=220)
+    st.image("chalex_network.png", width=400)
 except Exception:
     pass
 
